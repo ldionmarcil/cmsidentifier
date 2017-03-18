df501900112a661e4ad755e5c9b6e42ccc0fc210
Louis Dion-Marcil <maden.ldm@gmail.com>
2017-03-18 17:22:33 -0400

    Fixed YAML parsing

--- pentets/pentets.py
+++ pentets/pentets.py
@@ -1,7 +1,26 @@
+import os
+import pdb
+import argparse
+from rule import *
+
 def run():
-    rules = load_rules("../rules/*.yml")
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-u', '--url', metavar="URL", help="Root URL to scan", required=True)
+    parser.add_argument('-f', '--cms-family', metavar="FAMILY", help="The family of the CMS to scan for")
+    parser.add_argument('-o', '--output', nargs=1, type=str, metavar="URL", help="The path to the report to produce, if desired")
+    parser.add_argument('-r', '--rules', nargs=1, type=str, metavar="DIRECTORY", help="The path to the directory where rules reside")
+    arguments = parser.parse_args()
+
+
+    if arguments.rules is not None:
+        RULE_FOLDER = arguments.rules[0]
+    else:
+        RULE_FOLDER = os.path.dirname(os.path.realpath(__file__)) + "/../rules"
+
+    rules = load_rules(RULE_FOLDER + "/*.yml")
+
     for rule in rules:
-        print("Executing rule %s".format(rule.name))
+        print("Executing rule %s" % (rule.name))
         rule.execute()
 
 if __name__ == '__main__':
--- pentets/rule.py
+++ pentets/rule.py
@@ -1,17 +1,19 @@
 import yaml
 import glob
+import pdb
 
 class Rule(yaml.YAMLObject):
     yaml_tag = u'!Rule'
 
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, yaml_obj):
+        self.yaml_obj = yaml_obj
+        self.name = yaml_obj.name
 
     def execute(self):
         pass
 
 def load_rule(file):
-    return Rule(yaml.load(file))
+    return Rule(yaml.load(open(file,'r')))
 
 def load_rules(search_glob):
-    return map(load_rules, glob.glob(search_glob))
+    return map(load_rule, glob.glob(search_glob))
