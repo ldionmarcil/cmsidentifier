4020d12436109e8eda31670203bfed4ad1a51d59
Louis Dion-Marcil <maden.ldm@gmail.com>
2017-03-19 01:39:09 -0400

    Added passive and active skeleton class

--- pentets/rule.py
+++ pentets/rule.py
@@ -5,15 +5,27 @@ import pdb
 class Rule(yaml.YAMLObject):
     yaml_tag = u'!Rule'
 
-    def __init__(self, yaml_obj):
-        self.yaml_obj = yaml_obj
-        self.name = yaml_obj.name
-
+    def __init__(self, name):
+        self.name = name
+        
     def execute(self):
         pass
 
+class Passive(yaml.YAMLObject):
+    yaml_tag = u'!Passive'
+
+    def __init__(self, test):
+        self.test = test
+
+class Active(yaml.YAMLObject):
+    yaml_tag = u'!Active'
+
+    def __init__(self, test):
+        self.test = test
+            
+    
 def load_rule(file):
-    return Rule(yaml.load(open(file,'r')))
+    return yaml.load_all(open(file,'r'))
 
 def load_rules(search_glob):
     return map(load_rule, glob.glob(search_glob))
