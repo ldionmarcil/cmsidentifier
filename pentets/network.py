ad947b618ac5efb66521d9c2bf4907b3212ba4d7
Louis Dion-Marcil <maden.ldm@gmail.com>
2017-03-21 21:44:05 -0400

    Keep a trace of networking

--- pentets/network.py
+++ pentets/network.py
@@ -1,8 +1,10 @@
+import logging
 import pycurl
 from io import BytesIO
 
 
 def request(url):
+    logging.debug("Network: %s" %url)
     buffer = BytesIO()
     c = pycurl.Curl()
     c.setopt(c.URL, url)
