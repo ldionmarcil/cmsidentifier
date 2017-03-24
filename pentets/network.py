54934edf0a9ae93d3db5523bf18e1c7574145689
Louis Dion-Marcil <maden.ldm@gmail.com>
2017-03-23 23:31:12 -0400

    Actually use UA + default to most common UA

--- pentets/network.py
+++ pentets/network.py
@@ -10,7 +10,7 @@ def request(url, user_agent):
     c.setopt(c.URL, url)
     c.setopt(c.WRITEDATA, buffer)
     c.setopt(c.FOLLOWLOCATION, True)
-    c.setopt(c.USERAGENT, "Mozilla/5.0 (Windows NT 5.1; rv:5.0.1) Gecko/20100101 Firefox/5.0.1")
+    c.setopt(c.USERAGENT, user_agent)
     c.perform()
     c.close()
     return buffer.getvalue()
