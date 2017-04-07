ed627de8899fff9c9b7e3d3000a9b2c1224ae83b
Maxime Bedard <maxime.bedard@shopify.com>
2017-04-07 16:22:08 -0400

    yolo fix broken tests

--- tests/report_test.py
+++ tests/report_test.py
@@ -3,8 +3,9 @@ from io import StringIO
 from pentets.report import *
 
 class MockedScan():
-    def something(self):
-        return "monsieur l'ordinateur"
+    targets = "monsieur l'ordinateur"
+    curl_client = None
+    succeded_rulesets = None
 
 class ReportTest(unittest.TestCase):
     def test_render_from_file(self):
@@ -12,7 +13,7 @@ class ReportTest(unittest.TestCase):
         self.assertEqual("allo: monsieur l'ordinateur", report.render())
 
     def test_render(self):
-        report = Report("asdfg: {{ scan.something() }}", MockedScan())
+        report = Report("asdfg: {{ targets }}", MockedScan())
         self.assertEqual("asdfg: monsieur l'ordinateur", report.render())
 
     def test_dump(self):
